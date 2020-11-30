d193186d2cb6532794e9b07893f2a57824202013
Prashanth <67106624+pchandrasekaran1595@users.noreply.github.com>
2020-12-01 01:46:51 +0530

    Update fit_predict.py

--- Ionosphere Data/fit_predict.py
+++ Ionosphere Data/fit_predict.py
@@ -7,8 +7,8 @@ from time import time
 from Dataset import Dataset
 from MLP import MLP
 
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
+#torch.backends.cudnn.deterministic = True
+#torch.backends.cudnn.benchmark = False
 
 def breaker():
     print("\n" + 50*"-" + "\n")
